cdac38de9485e2b01b01e6eeff23540d48eda208
dcwik96 <greek96@interia.pl>
2016-02-26 16:17:47 +0000

    Update zad06.c

--- lab01/zad06.c
+++ lab01/zad06.c
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
-#include <stdio.h>
-
 int main()
 {
         int a,b,c,d;
         a=b=c=d=0;
         printf("A. Wartosci: %d %d %d %d\n",a,b,c,d);
-        //Zmiennym a,b,c,d przypisana jest wartosc 0
+        //Zmiennym a,b,c,d przypisano wartosc 0
         int i,N=5,tab[5]={0,1,2,3,4};
         for(i=0;i<N;tab[i++]=i)
                 printf("%d ",tab[i]);
@@ -21,27 +18,4 @@ int main()
         //i jest mnozone przez 2 dopoki jest mniejsze od N
         return 0;
 }
-=======
-#include <stdio.h>
 
-int main()
-{
-        int a,b,c,d;
-        a=b=c=d=0;
-        printf("A. Wartosci: %d %d %d %d\n",a,b,c,d);
-        //Zmiennym a,b,c,d przypisana jest wartosc 0
-        int i,N=5,tab[5]={0,1,2,3,4};
-        for(i=0;i<N;tab[i++]=i)
-                printf("%d ",tab[i]);
-        //i-ty indeks tab[] przyjmuje wartosc i oraz zwieksza sie o jeden
-        printf("\n");
-        for(i=0;i<N;tab[++i]=i)
-                printf("%d ",tab[i]);
-        //i-ty indeks tab[] zwieksza sie o jeden i przyjmuje wartosc i
-        i=1;
-        while((i*=2)<N);
-        printf("\n%d\n",i);
-        //i jest mnozone przez 2 dopoki jest mniejsze od N
-        return 0;
-}
->>>>>>> 32a2cc49ab373eeedcbccb949765efbdc6b22a0a
