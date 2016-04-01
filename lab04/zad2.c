aea00772a31531d4d86a01d62dc8518d0412ee16
dcwik96 <greek96@interia.pl>
2016-04-01 10:24:33 +0200

    Update zad2.c

--- lab04/zad2.c
+++ lab04/zad2.c
@@ -1,132 +1,145 @@
-//Poniższa tożsamość Eulera w prosty sposób łączy ze sobą 5 ważnych stałych:
-//    eπi + 1  =  0
-//dlatego przez niektórych jest uważana za ,,najpiękniejszy'' wzór matematyczny.
-//Program z zadania 1 wzbogacić o funkcje potrzebne do tego, żeby można było sprawdzić, czy tożsamość Eulera istotnie zachodzi (mogą wystąpić błędy zaokrągleń).
-//Podnoszenie e do potęgi urojonej można zdefiniować tak:
-//    eri  =  cos(r) + sin(r)i
-//Podnoszenie e do potęgi zespolonej (niekoniecznie czysto urojonej) można zdefiniować tak:
-//    ez  =  ∑k=0∞ zk/k! 
+// Poniższa tożsamość Eulera w prosty sposób łączy ze sobą 5 ważnych stałych:
+// eπi + 1  =  0.
+// Dlatego przez niektórych jest uważana za ,,najpiękniejszy'' wzór matematyczny.
+// Program z zadania 1 wzbogacić o funkcje potrzebne do tego, żeby można było sprawdzić,
+// czy tożsamość Eulera istotnie zachodzi (mogą wystąpić błędy zaokrągleń).
 
-#include<stdio.h>
-#include<math.h>
-#include<ctype.h>
-#include<stdlib.h>
-
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <complex.h>
+//LICZBA EULERA
+#define E 2.71828182845904523536;
+#define M_PI 3.14159265358979323846
 /****************************************************************/
 // DEFINICJA TYPU LICZB ZESPOLONYCH:
-
 typedef struct
 {
   double rea, ima;
-}  zespol;
-
+} zespol;
 /****************************************************************/
 // POMOCNICZE:
-
-void  err(char s[])
-{  // -- sygnalizacja bledu wejscia
-  printf("\n !!! FUNKCJA zesp_get: %s !!!\n\n", s);
+void err(char s[])
+{
+  // -- sygnalizacja bledu wejscia
+  printf("\n!!! FUNKCJA zesp_get: %s !!!\n\n", s);
   exit(1);
 }
-
 /****************************************************************/
 // INICJALIZACJE oraz WEJSCIE-WYJSCIE:
-
-zespol  zesp_zestawic (double r, double i)
+zespol zesp_zestawic(double r, double i)
 {
-    // zestawienie liczby zespolonej z dwoch rzeczywistych
+  // zestawienie liczby zespolonej z dwoch rzeczywistych
   zespol z;
-  z.rea = r;  z.ima = i;
+  z.rea=r;
+  z.ima=i;
   return z;
 }
-
-zespol  zesp_get (void)
+zespol zesp_get(void)
 {
-    /* wczytanie liczby zespolonej; powinna skladac sie z dwoch
-       rzeczywistych, oraz plusa lub minusa miedzy nimi, zaczynac sie od
-       nawiasu otwierajacego, konczyc sie litera ,,i'' oraz nawiasem
-       zamykajacym;
-       np.  (123.45 + 67.89i)
-    */
-  char ch;  zespol z;  int znak_im;
-  do { ch = getchar(); } while (isspace(ch));
-  if (ch == '(') {
-    if (scanf("%lf", &(z.rea)) == 1) {
-      do { ch = getchar(); } while (isspace(ch));
-      if (ch == '+' || ch == '-') {
-	if (ch == '+')  znak_im = 1;
-	else  znak_im = -1;
-	if (scanf("%lf", &(z.ima)) == 1) {
-	  do { ch = getchar(); } while (isspace(ch));
-	  if (ch == 'i') {
-	    do { ch = getchar(); } while (isspace(ch));
-	    if (ch == ')') {
-	      if (znak_im == -1)  z.ima = -z.ima;
-	    } else err("brak koncowego nawiasu");
-	  } else err("brak 'i' na koncu czesci urojonej");
-	} else err("niepoprawna czesc urojona");
+  /* wczytanie liczby zespolonej; powinna skladac sie z dwoch
+  rzeczywistych, oraz plusa lub minusa miedzy nimi, zaczynac sie od
+  nawiasu otwierajacego, konczyc sie litera ,,i'' oraz nawiasem
+  zamykajacym;
+  np. (123.45 + 67.89i) */
+  char ch;
+  zespol z;
+  int znak_im;
+  do
+  {
+    ch=getchar();
+  } while(isspace(ch));
+  if(ch=='(')
+  {
+    if(scanf("%lf", &(z.rea))==1)
+    {
+      do
+      {
+        ch = getchar();
+      } while(isspace(ch));
+      if(ch=='+' || ch=='-')
+      {
+        if(ch=='+') znak_im=1;
+        else znak_im=-1;
+        if(scanf("%lf", &(z.ima))==1)
+        {
+          do
+          {
+            ch=getchar();
+          } while(isspace(ch));
+          if(ch=='i')
+          {
+            do
+            {
+              ch=getchar();
+            } while(isspace(ch));
+            if(ch==')')
+            {
+              if(znak_im==-1) z.ima=-z.ima;
+            } else err("brak koncowego nawiasu");
+          } else err("brak 'i' na koncu czesci urojonej");
+        } else err("niepoprawna czesc urojona");
       } else err("po czesci rzeczywistej brak znaku '+' lub '-'");
     } else err("niepoprawna czesc rzeczywista");
   } else err("brak rozpoczynajacego nawiasu");
   return z;
 }
-
-void  zesp_print (zespol z) { // drukowanie liczby zespolonej
-  if (z.ima >= 0)
-    printf("(%.2lf+%.2lfi)", z.rea, z.ima);
-  else
-    printf("(%.2lf-%.2lfi)", z.rea, -z.ima);
+void zesp_print(zespol z)
+{
+  // drukowanie liczby zespolonej
+  if(z.ima>=0) printf("(%.2lf+%.2lfi)", z.rea, z.ima);
+  else printf("(%.2lf-%.2lfi)", z.rea, -z.ima);
 }
-
 /****************************************************************/
 // DZIALANIA:
-
-zespol  zesp_dodac (zespol z1, zespol z2)
+zespol zesp_dodac(zespol z1, zespol z2)
 {
-	zespol wynik;
-	wynik.rea = z1.rea + z2.rea;
-	wynik.ima = z1.ima + z2.ima;
-	return wynik;
-	}
-
-zespol  zesp_odjac (zespol z1, zespol z2)
-{
-	zespol wynik;
-	wynik.rea = z1.rea - z2.rea;
-	wynik.ima = z1.ima - z2.ima;
-	return wynik;
+  zespol wyn;
+  wyn.rea=z1.rea+z2.rea;
+  wyn.ima=z1.ima+z2.ima;
+  return wyn;
 }
-  // odejmowanie liczb zespolonych
-
-zespol  zesp_razy (zespol z1, zespol z2)
+// dodawanie liczb zespolonych
+zespol zesp_odjac(zespol z1, zespol z2)
 {
-	zespol wynik;
-	wynik.rea = z1.rea * z2.rea - z1.ima * z2.ima;
-  wynik.ima = z1.rea * z2.ima + z1.ima * z2.rea;
-  return wynik;
+  zespol wyn;
+  wyn.rea=z1.rea-z2.rea;
+  wyn.ima=z2.ima-z2.ima;
+  return wyn;
 }
-  // mnozenie liczb zespolonych
-
-zespol  zesp_sprzez (zespol z)
+// odejmowanie liczb zespolonych
+zespol zesp_razy(zespol z1, zespol z2)
 {
-	zespol wynik;
-  wynik.ima = -z.ima;
-  return wynik;
-
+  zespol wyn;
+  wyn.rea=z1.rea*z2.rea-z1.ima*z2.ima;
+  wyn.ima=z1.rea*z2.ima+z1.ima*z2.rea;
+  return wyn;
 }
-  // liczba sprzezona do danej zespolonej
-
-double  zesp_abs (zespol z)
+// mnozenie liczb zespolonych
+zespol zesp_dziel(zespol z1, int dzielnik)
 {
-	double wynik;
-  wynik = sqrt(z.rea * z.rea + z.ima * z.ima);
-  return wynik;
-
+  zespol wyn;
+  wyn.rea=z1.rea/dzielnik;
+  wyn.ima=z1.ima/dzielnik;
+  return wyn;
 }
-  // wartosc bezwzgledna liczby zespolonej
-
+// dzielenie liczb zespolonych
+zespol zesp_sprzez(zespol z)
+{
+  zespol wyn;
+  wyn.ima=-z.ima;
+  return wyn;
+}
+// liczba sprzezona do danej zespolonej
+double zesp_abs(zespol z)
+{
+  double wyn;
+  wyn=sqrt(z.rea*z.rea+z.ima*z.ima);
+  return wyn;
+}
+// wartosc bezwzgledna liczby zespolonej
 /****************************************************************/
-
 zespol euler()
 {
   zespol z;
@@ -161,19 +174,19 @@ zespol zesp_exp(zespol z)
   return wyn;
 }
 // Funkcje Eulera
-
-int main ()
+int main()
 {
-  zespol  z1, z2;
-
-  printf("\n z1 == "); z1 = zesp_get();
-  printf(" z2 == "); z2 = zesp_get();
-
-  printf("\n z1+z2 == "); zesp_print(zesp_dodac(z1, z2));
-  printf("\n z1-z2 == "); zesp_print(zesp_odjac(z1, z2));
-  printf("\n z1*z2 == "); zesp_print(zesp_razy(z1, z2));
-  printf("\n |z1+z2| == %.4lf\n\n", zesp_abs(zesp_dodac(z1, z2)));
-
+  zespol z1, z2;
+  zespol Eul=euler();
+  zespol O=jeden();
+  zespol PII={0.0, M_PI};
+  zespol left=zesp_dodac(zesp_exp(PII), O);
+  printf("Z1 == "); z1 = zesp_get();
+  printf("Z2 == "); z2 = zesp_get();
+  printf("\nZ1+Z2 == "); zesp_print(zesp_dodac(z1, z2));
+  printf("\nZ1-Z2 == "); zesp_print(zesp_odjac(z1, z2));
+  printf("\nZ1*Z2 == "); zesp_print(zesp_razy(z1, z2));
+  printf("\n|Z1+Z2| == %.4lf\n", zesp_abs(zesp_dodac(z1, z2)));
   printf("Tozsamosc Eulera: \n");
   zesp_print(Eul);
   printf(" + ");
