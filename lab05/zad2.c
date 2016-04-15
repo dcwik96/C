43b6a67bcb64b93c9d0b4904bf79139fded6641b
dcwik96 <greek96@interia.pl>
2016-04-15 10:21:32 +0200

    Update zad2.c

--- lab05/zad2.c
+++ lab05/zad2.c
@@ -1,3 +1,4 @@
+#include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
@@ -6,81 +7,66 @@
 #define NAZW_MAX 15
 #define IL_OSOB 10
 
-typedef struct
-{
-    char imie[IMIE_MAX+1];
-    char nazwisko[NAZW_MAX+1];
-    int pensja;
+typedef struct {
+  char imie[IMIE_MAX+1];
+  char nazwisko[NAZW_MAX+1];
+  int pensja;
 } osoba;
 
 osoba spis[IL_OSOB];
 
 //=======================================================
 
-void  utworz_spis(void)
+void  utworz_spis(char *name)
 {
-    FILE* baza = fopen("nieposortowane.txt","r");
-    if (baza == NULL) printf("\n ZLE\n\n");
-    int i;
-    for (i=0; i<IL_OSOB; i++)
-    {
-        fscanf(baza, "%s", spis[i].imie);
-        fscanf(baza, "%s", spis[i].nazwisko);
-        fscanf(baza, "%i", &spis[i].pensja);
-        }
-    fclose(baza);
+  FILE* baza =
+    fopen(name, "r");
+  if (baza == NULL) printf("\n ZLE\n\n");
+  int i;
+  for (i=0; i<IL_OSOB; i++) {
+  	fscanf(baza, "%s", spis[i].imie);
+    fscanf(baza, "%s", spis[i].nazwisko);
+    fscanf(baza, "%i", &spis[i].pensja);
+  }
+  fclose(baza);
 }
-
-//=======================================================
-
-int porownanie (const void * a,const void * b)
+int my_compare (const void * a, const void * b)
 {
     osoba *osoba_a, *osoba_b;
-    osoba_a=(osoba*)a;
-    osoba_b=(osoba*)b;
+    osoba_a = (osoba*)a;
+    osoba_b = (osoba*)b;
 
-    return strcmp(osoba_a->nazwisko,osoba_b->nazwisko);
+    return strcmp(osoba_a->nazwisko, osoba_b->nazwisko);
 }
-
+//=======================================================
 void  sortuj_spis(void)
 {
-  /* sortuje  spis  alfabetycznie wg nazwisk,
-     a w przypadku rownych nazwisk wg imion
-  */
-
-    qsort(spis,IL_OSOB,sizeof(osoba),porownanie);
-    FILE* baza2=fopen("posortowane.txt","w");
-    int i;
-    for (i=0;i<IL_OSOB;i++)
+	qsort(spis, IL_OSOB, sizeof(osoba), my_compare);
+	FILE* baza2=fopen("posortowane.txt","w");
+	int i;
+    for(i=0;i<IL_OSOB;i++)
     {
-        fprintf(baza2, "%20s    ",spis[i].imie);
-        fprintf(baza2, "%20s    ",spis[i].nazwisko);
-        fprintf(baza2, "%20i\n  ",spis[i].pensja);
-
+       fprintf(baza2, "%20s	", spis[i].imie);
+       fprintf(baza2, "%20s	", spis[i].nazwisko);
+       fprintf(baza2, "%20i\n", spis[i].pensja);
     }
-
+    fclose(baza2);
 }
-
 //=======================================================
 
-int  znajdz_nazwisko (char na[NAZW_MAX+1],char im[IMIE_MAX+1], int *p)
+int  znajdz_nazwisko (char na[NAZW_MAX+1], char im[IMIE_MAX+1], int *p)
 {
-  /* do danego nazwiska  na  znajduje w spisie
-     imie  im  oraz pensje  p
-     jesli znajdzie, to zwraca 1, jesli nie, to 0
-  */
-
-    int i;
-    for (i=0;i<IL_OSOB;i++)
-    {
-        if(strcmp(spis[i].nazwisko,na)==0)
-        {
-            strcpy(na,spis[i].nazwisko);
-            *p=spis[i].pensja;
-            return 1;
-        }
+	osoba* a=(osoba*)bsearch(na, spis, IL_OSOB, sizeof(osoba), my_compare);
+	if(a)
+	{
+		strcpy(im,a->imie);
+    	*p=a->pensja;
+    	return 1;
     }
+	else
     return 0;
+
+
 
 }
 
@@ -88,68 +74,65 @@ int  znajdz_nazwisko (char na[NAZW_MAX+1],char im[IMIE_MAX+1], int *p)
 
 int  znajdz_imie (char im[NAZW_MAX+1], char na[IMIE_MAX+1], int *p)
 {
-  /* do danego imienia  im  znajduje w spisie
-     nazwisko  na  oraz pensje  p
-     jesli znajdzie, to zwraca 1, jesli nie, to 0
-  */
 
-    int i;
-    for (i=0;i<IL_OSOB;i++)
-    {
-        if(strcmp(spis[i].imie,im)==0)
-        {
-            strcpy(na,spis[i].nazwisko);
-            *p=spis[i].pensja;
-            return 1;
-        }
-    }
-    return 0;
+  	int i;
+	for(i=0; i<IL_OSOB; i++)
+	{
+		if(strcmp(spis[i].imie,im) == 0)
+		{
+			strcpy(na,spis[i].nazwisko);
+			*p = spis[i].pensja;
+			return 1;
+		}
+	}
 
-}
+	return 0;
+  }
 
 //=======================================================
 
-int main ()
+int main (int ile_arg, char* arg[])
 {
-    char odpowiedz, im[NAZW_MAX+1], na[IMIE_MAX+1];
-    int p;
+ if(ile_arg == 2)
+ {
+  char ch[100];
+  sscanf(arg[1], "%s", ch);
+  char odpowiedz, im[NAZW_MAX+1], na[IMIE_MAX+1];
+  int p;
 
-    utworz_spis(); sortuj_spis();
+  utworz_spis(ch); sortuj_spis();
 
-    do
-    {
-        printf("\n Znalezc wg imienia (I), nazwiska (N), czy zakonczyc (Q)? ");
-        do
-        {
-            odpowiedz = getchar();
-        }while (isspace(odpowiedz));
+  do {
+    printf(
+     "\n Znalezc wg imienia (I), nazwiska (N), czy zakonczyc (Q)? "
+    );
+    do { odpowiedz = getchar(); }
+    while (isspace(odpowiedz));
+    odpowiedz = tolower(odpowiedz);
+    switch (odpowiedz) {
+    case 'i' :
+      printf("\n szukane imie: ");
+      scanf("%s", im);
+      if (znajdz_imie(im, na, &p))
+	printf(" IMIE: %s, NAZWISKO: %s, PENSJA: %i\n", im, na, p);
+      else  printf(" nie ma imienia %s\n", im);
+      break;
+    case 'n' :
+      printf("\n szukane nazwisko: ");
+      scanf("%s", na);
+      if (znajdz_nazwisko(na, im, &p))
+	printf(" IMIE: %s, NAZWISKO: %s, PENSJA: %i\n", im, na, p);
+      else  printf(" nie ma nazwiska %s\n", im);
+      break;
+    case 'q' : break;
+    default :
+      printf(" Poprawne odpowiedzi: N, I, Q.\n");
+    }
+  }  while (tolower(odpowiedz) != 'q');
 
-        odpowiedz = tolower(odpowiedz);
-
-        switch (odpowiedz)
-        {
-            case 'i' :
-                printf("\n szukane imie: ");
-                scanf("%s", im);
-                if (znajdz_imie(im, na, &p))
-                    printf(" IMIE: %s, NAZWISKO: %s, PENSJA: %i\n", im, na, p);
-                else
-                    printf(" nie ma imienia %s\n", im);
-                break;
-            case 'n' :
-                printf("\n szukane nazwisko: ");
-                scanf("%s", na);
-                if (znajdz_nazwisko(na, im, &p))
-                    printf(" IMIE: %s, NAZWISKO: %s, PENSJA: %i\n", im, na, p);
-                else
-                    printf(" nie ma nazwiska %s\n", na);
-                break;
-            case 'q' : break;
-                default :
-            printf(" Poprawne odpowiedzi: N, I, Q.\n");
-        }
-    } while (tolower(odpowiedz) != 'q');
-
-    printf("\n DZIEKUJE.\n\n");
-    return 0;
+  printf("\n DZIEKUJE.\n\n");
+  return 0;
+ }
+ else printf("\nNiepoprawne wywolanie programu! ./zad2b nazwapliku/lokalizacjapliku!\n");
+ return 0;
 }
