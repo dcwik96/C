2b294981e50a1a2ef09d36d42d692c428f3d1158
dcwik96 <greek96@interia.pl>
2016-04-17 17:45:22 +0200

    Update zad05.c

--- lab01/zad05.c
+++ lab01/zad05.c
@@ -11,8 +11,10 @@
 
 int main()
 {
-int a, b;
-a=2; b=4;
-if ((a=2) & (b=4))   printf(" TAK\n");
-else   printf(" NIE\n");
+  int a, b;
+  a=2; b=4;
+  if ((a=2) & (b=4))   printf(" TAK\n");
+  else   printf(" NIE\n");
 }
+
+//Program wypisze "NIE"
