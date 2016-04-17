ede0bfede2393efe7d2ba87ac9fa2eaed83ba018
dcwik96 <greek96@interia.pl>
2016-04-17 17:37:51 +0200

    Update zad03.c

--- lab01/zad03.c
+++ lab01/zad03.c
@@ -6,34 +6,59 @@
 
 #include <stdio.h>
 
-void dziwne_for(int tab[], int size)
+void petla_for_i_plus_plus(int tab[], int size)
 {
 	int i;
 	for(i=0;i<size;)
 		printf("%i ",tab[i++]);
+    printf("\n Wartosci i : \t");
+    for(i=0;i<size;)
+        printf("%i ",i++);
 }
 
-void dziwne2_for(int tab[], int size)
+void petla_for_plus_plus_i(int tab[], int size)
 {
 	int i;
 	for(i=0;i<size;)
 		printf("%i ",tab[++i]);
+    printf("\n Wartosci i : \t");
+    for(i=0;i<size;)
+        printf("%i ",++i);
 }
+
+void zmienne ( int x )
+{
+    int z=0,y=0,g=0;
+    printf("\t--test 2\n");
+    printf("y=%i x=%i",y,x); //przed dzialaniem
+    y = (x+=2);
+    printf("\ny= %i x=%i\n",y,x); //po dzialaniu
+
+    printf("\t--test 3\n");
+    printf("z=%i x=%i",z,x); //przed dzialaniem
+    z = (x %= 3) + (x %= 4);
+    printf("\nz= %i x=%i\n",z,x); //po dzialaniu
+
+    printf("\t--test 4*\n");
+    printf("g=%i x=%i",g,x); //przed dzialaniem
+    g = (x*=2);
+    printf("\ng= %i x=%i\n",g,x); //po dzialaniu
+}
+
+
 
 int main()
 {
-	int tab[4]={10,11,12,13};
+    int tab[5] = {1,2,3,4,5};
 
-	for(int i=0;i<4;i++)
-		printf("%i ",tab[i]);
+    printf("Roznica w dzialaniu i++ / ++i :\n");
+    petla_for_i_plus_plus(tab,5);
+    printf("\n");
+    petla_for_plus_plus_i(tab,5);
 
-	printf("\n");
-
-	dziwne_for(tab,4);
-	printf("\n\n");
-
-  dziwne2_for(tab,4);
-  printf("\n\n");
+    printf("\n\nRoznica w przypisywaniu wartosci zmiennym : \n");
+    zmienne(5);
 
 	return 0;
 }
+
