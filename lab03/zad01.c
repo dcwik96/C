5a4f85c53286eada3cd910029cc88b92afd80656
dcwik96 <greek96@interia.pl>
2016-04-26 18:25:41 +0200

    Update zad01.c

--- lab03/zad01.c
+++ lab03/zad01.c
@@ -1,24 +1,21 @@
-//Napisać program, który bada poznane typy liczbowe w C; t.zn. ustala
-//    ilość bajtów, przeznaczonych na zmienną (operator sizeof),
-//    największą i najmniejszą wartość liczby,
-//    ,,ziarno'', czyli najmniejszą taką liczbę x, że 1.0+x≠1.0 ,
-//    ,,precyzję'', czyli maksymalną liczbę cyfr dziesiętnych po kropce 
+// Napisać program, który bada poznane typy liczbowe w C, tzn. ustala:
+// * ilość bajtów, przeznaczonych na zmienną (operator sizeof),
+// * największą i najmniejszą wartość liczby,
+// * ,,ziarno'', czyli najmniejszą taką liczbę x, że 1.0+x≠1.0 ,
+//* ,,precyzję'', czyli maksymalną liczbę cyfr dziesiętnych po kropce.
 
 #include <stdio.h>
 #include <limits.h>
 #include <float.h>
-
 int main()
 {
-  printf("Typ\t\tBajty\t\tDolnaWartosc\t\t\tGornaWartosc\t\t\tZiarno\t\t\tPrecyzja\n");
-  printf("short\t\t%li\t\t%i\t\t\t%i\t\t\t-\t\t\t-\n",sizeof(short),SHRT_MIN,SHRT_MAX);
-  printf("int\t\t%li\t\t%i\t\t\t%i\t\t\t-\t\t\t-\n",sizeof(int),INT_MIN,INT_MAX);
-  printf("long\t\t%li\t\t%li\t\t\t%li\t\t\t-\t\t\t-\n",sizeof(long),LONG_MIN,LONG_MAX);
-  printf("long long\t\t%li\t\t%lli\t\t\t%lli\t\t\t-\t\t\t-\n",sizeof(long long),LLONG_MIN,LLONG_MAX);
-
-  printf("float\t\t%e\t\t%e\t\t\t%e\t\t\t%e\t\t\t%e\n",sizeof(float),LLONG_MIN,LLONG_MAX,FLT_EPSILON,FLT_DIG);
-  printf("double\t\t%le\t\t%le\t\t\t%le\t\t\t%le\t\t\t%le\n",sizeof(double),LLONG_MIN,LLONG_MAX,DBL_EPSILON,DBL_DIG);
-  printf("long double\t\t%Le\t\t%Le\t\t\t%Le\t\t\t%Le\t\t\t%Le\n",sizeof(long double),LLONG_MIN,LLONG_MAX,LDBL_EPSILON,LDBL_DIG);
-
-return 0;
+  printf("|%11s|%8s|%21s|%21s|%13s|%10s|\n","TYP","DOL_WART","GOR_WART","ZIARNO","PRECYZJA","FORMAT");
+  printf("|%11s|%8i|%21i|%21i|%13i|%10s|\n","short",(short)SHRT_MIN,(short)SHRT_MAX,' ',' ',"%i");
+  printf("|%11s|%8i|%21i|%21i|%13i|%10s|\n","int",(int)INT_MIN,(int)INT_MAX,' ',' ',"%i");
+  printf("|%11s|%8li|%21li|%21li|%13li|%10s|\n","long",(long)LONG_MIN,(long)LONG_MAX,' ',' ',"%li");
+  printf("|%11s|%8lli|%21lli|%21lli|%13lli|%10s|\n","long long",(long long)LLONG_MIN,(long long)LLONG_MAX,' ',' ',"%lli");
+  printf("|%11s|%8e|%21e|%21e|%13e|%10s|\n","float", (float)FLT_MIN, (float)FLT_MAX,FLT_EPSILON,(float)FLT_DIG,"%e");
+  printf("|%11s|%8le|%21le|%21le|%13le|%10s|\n","double", (double)DBL_MIN, (double)DBL_MAX,DBL_EPSILON,(double)DBL_DIG,"%le");
+  printf("|%11s|%8Le|%21Le|%21Le|%13Le|%10s|\n","long double",(long double)LDBL_MIN,(long double)LDBL_MAX,LDBL_EPSILON,(long double)LDBL_DIG,"%Le");
 }
+//ERROR
