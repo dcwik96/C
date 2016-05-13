2cd11f37edcc3798ddc7d64af3e1b3741e4e817e
dcwik96 <greek96@interia.pl>
2016-05-13 10:54:51 +0200

    Update lista.php

--- lab09/lista.php
+++ lab09/lista.php
@@ -1,39 +1,60 @@
 <!doctype html>
-<html land="pl">
-	<head>
-		<meta charset="utf-8"/>
-		<title>Lista osob chetnych na impreze</title>
-	</head>
-	
-	<body>
-		<div style="float:left; vertical-align:top; background-color:red">
-			
-			<h3>Chetni:</h3>
-			
-			<?php
-			if(isset($_SESSION['indeks']))
-				for($i=1; $i<=$_SESSION['indeks']; $i++)
-				{
-					print('$i');
-					print('. ');
-					print('$_SESSION');
-					print('<br>');
-				}
-			?>
-			
-		</div>
-		
-		<div style="float:right; vertical-align:top; background-color:yellow">
-			
-			<h3>Zapisz sie!</h3>
-			
-			<form action="zapis.php">
-				<input type="text" name="nowy">
-				<input type="hidden" name="ile" value="<?php if(isset($_SESSION['indeks'])) echo $_SESSION['indeks']; else echo '0';?>">
-				<input type="submit" value="ZAPISZ">
-			</form>
-			
-		</div>
-		
-	</body>
+
+<html>
+
+<head>
+ <title>chetni na impreze</title>
+</head>
+
+<body>
+
+<div style="float:left; vertical-align:top; background-color:yellow">
+ <h3>Chetni na impreze:</h3>
+ <h5>
+ <?php
+ if ($_REQUEST['ile'] != NULL)
+ {
+	for ($i = 0; $i <= $_REQUEST['ile']; $i++)
+	{
+		if($i< $_REQUEST['ile'] )
+		{
+			$a= $_REQUEST['imie'.$i] ;
+			echo $i+1 .". ".$a."<br>";
+		}else
+		{
+			echo $i+1 .". ".$_REQUEST['nowy']."<br>" ;
+		}
+	} 
+}
+ ?>
+ </h5>
+
+ </div>
+<div style="float:right; vertical-align:top">
+ <h3>Nowa osoba:</h3>
+
+  <form action="">
+  <input type="text" name="nowy">
+  <?php
+  if($_REQUEST['ile']!=NULL)
+  {
+	for($i=0;$i<=$_REQUEST['ile'];$i++)
+	{
+		if($i< $_REQUEST['ile'] )
+		{
+			$a=$_REQUEST['imie'.$i];
+			echo '<input type="hidden" name="imie'.$i.'" value="'.$a.'" />'."\n";
+		}else
+		{
+			echo '<input type="hidden" name="imie'.$i.'" value="'.$_REQUEST['nowy'].'" />'."\n";
+		}
+	}
+ }
+?>
+  <input type ="hidden" name="ile" value="<?php if($_REQUEST['ile']==NULL){ echo "0";}if($_REQUEST['ile']!=NULL){ $a= $_REQUEST['ile']; echo $a+1;}?>">
+ </form>
+</div>
+
+</body>
+
 </html>
